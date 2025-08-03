40e26fbf334a521ad873bd41b46a1901b645f46a
work arfat <work.arfat@gmail.com>
2025-08-03 11:40:27 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,27 @@ import { data } from "@/data";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-6">
-      <ApexCandleChart data={data} className="w-full max-w-5xl px-5 mx-auto" />
+    <main className="flex flex-col min-h-screen p-6">
+      <div className="flex flex-col items-center justify-center flex-1">
+        <ApexCandleChart
+          data={data}
+          className="w-full max-w-5xl px-5 mx-auto"
+        />
+      </div>
+
+      <footer className="py-4 mt-6 text-sm text-center text-gray-600 border-t border-gray-200">
+        <p>
+          Built by{" "}
+          <a
+            href="https://github.com/mdarfatwork"
+            target="_blank"
+            className="font-medium text-blue-600 hover:underline"
+          >
+            Mohammed Arfat
+          </a>{" "}
+          for TechnoTouch Infotech
+        </p>
+      </footer>
     </main>
   );
 }
